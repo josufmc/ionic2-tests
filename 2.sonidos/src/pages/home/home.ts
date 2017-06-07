f884f639c2f62a508fb78fdc381f8793b8782fd9
josufmc@hotmail.com <josufmc@hotmail.com>
2017-06-07 21:56:24 +0200

    Sonidos app updated with sound effects, pause and replay

--- 2.sonidos/src/pages/home/home.ts
+++ 2.sonidos/src/pages/home/home.ts
@@ -1,5 +1,6 @@
+import { Animal } from './../../interfaces/animal.interface';
+import { ANIMALES } from './../../data/data.animales';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] = [];
+  audio = new Audio();
+  audioTiempo:any;
 
+  constructor() {
+    this.animales = ANIMALES.slice(0);
+  }
+
+  public reproducir(animal:Animal){
+    this.pausarAudio(animal);
+    if (animal.reproduciendo){
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log(animal);
+    this.audio.src = animal.audio;
+    animal.reproduciendo = true;
+    this.audio.load();
+    this.audio.play();
+
+    this.audioTiempo = setTimeout( ()=> {animal.reproduciendo = false}, animal.duracion * 1000);
+  }
+
+
+  private pausarAudio(animalSeleccionado:Animal){
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0; // Pasamos al principio del audio
+    for(let animal of this.animales){
+      if (animal.nombre != animalSeleccionado.nombre){
+        animal.reproduciendo = false;
+      }
+    }
   }
 
 }
