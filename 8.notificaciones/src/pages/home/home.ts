7ee7bbaa54651e4a35a0b5dd36b15d842922244a
josufmc@hotmail.com <josufmc@hotmail.com>
2017-07-20 19:11:41 +0200

    Notifications App: Added firebase messaging push test

---
+++ 8.notificaciones/src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Push, PushToken } from '@ionic/cloud-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public push: Push) {
+    this.inicializarNotificaciones();
+  }
+
+  private inicializarNotificaciones(){
+    this.push.register().then((t: PushToken) => {
+      return this.push.saveToken(t);
+    }).then((t: PushToken) => {
+      console.log('Token saved:', t.token);
+    }).catch((error)=>{
+      console.log('Error en el registro:', JSON.stringify(error));
+    });
+
+    this.push.rx.notification()
+      .subscribe((msg) => {
+        alert(msg.title + ': ' + msg.text);
+    });
+  }
+
+}
