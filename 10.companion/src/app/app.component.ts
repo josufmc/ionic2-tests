d4f0b83c8e192eb464a09c2f2adf084e5459d3fb
Josu <josufmc@hotmail.com>
2017-08-07 14:03:56 +0200

    Added tracker view

--- 10.companion/src/app/app.component.ts
+++ 10.companion/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  lat = 51.678418;
+  lng = 7.809007;
+
+  taxistas: any[] = [];
+  taxistaSeleccionado: any = {};
+  siguiendo = false;
+
+  constructor(private afDB: AngularFireDatabase) {
+    afDB.list('/usuarios').subscribe(
+      (taxistas) => {
+        this.taxistas = taxistas;
+        if (this.siguiendo) {
+          for (const taxista of taxistas){
+            if (taxista.$key === this.taxistaSeleccionado.$key) {
+              this.lat = taxista.lat;
+              this.lng = taxista.lng;
+            }
+          }
+        }
+      }
+    );
+  }
+
+  public seguirTaxista(taxista: any) {
+    this.lat = taxista.lat;
+    this.lng = taxista.lng;
+    this.taxistaSeleccionado = taxista;
+    this.siguiendo = true;
+    console.log(taxista);
+  }
+
+  public dejarSeguir() {
+    this.siguiendo = false;
+    this.taxistaSeleccionado = {};
+  }
 }
--- 10.companion/src/app/app.module.ts
+++ 10.companion/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { firebaseConfig } from './../config/firebase.config';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAcnUtoSed-P4zzs14JQsNQ8cuMIGGbh5w'
+    }),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
